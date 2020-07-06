a91bd3cb78f9e74c089a737b71edc4ad3af0aca9
Dominik <stelmaszynski.dominik@gmail.com>
2020-07-06 19:39:51 +0200

    Ex_1_solved list + sort

--- src/main/java/Zad_1/Main.java
+++ src/main/java/Zad_1/Main.java
@@ -1,4 +1,30 @@
 package Zad_1;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.*;
+
+
+//Stwórz metodę, która jako parametr przyjmuje listę stringów,
+// następnie zwraca tą listę posortowaną alfabetycznie od Z do A.
+
+
 public class Main {
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+        list.add("Dominik");
+        list.add("Beata");
+        list.add("Antosia");
+        list.add("Michał");
+        list.add("Paweł");
+
+        System.out.println("Lista - wersja oryginalna: " + list);
+
+        //do sortowania elementow
+        Collections.sort(list);
+        System.out.println("Lista posortowana: " + list);
+    }
 }
+
