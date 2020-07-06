08084112d2f66dd78dd6af91145b85409035dc2e
Dominik <stelmaszynski.dominik@gmail.com>
2020-07-06 21:08:56 +0200

    Ex_2 using streams. Need to fix sorting order

---
+++ src/main/java/Zad_2/Stream.java
@@ -0,0 +1,29 @@
+package Zad_2;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Stream {
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<>();
+        list.add("Bocian");
+        list.add("ala");
+        list.add("Kapcie");
+        list.add("zyrafa");
+        list.add("cel");
+
+        System.out.println("Lista - wersja oryginalna: " + list);
+        System.out.println("Lista - odwrotne sortowanie: " + sortedList(list));
+
+    }
+    private static List<String> sortedList (List<String>list){
+        return list
+                .stream()
+                .sorted(String.CASE_INSENSITIVE_ORDER)
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+    }
+}
