d5f896d7945774397fe80d5113764c602630ebed
Dominik <stelmaszynski.dominik@gmail.com>
2020-07-08 20:33:20 +0200

    cleaning

---
+++ src/main/java/Zad_4/Storage.java
@@ -0,0 +1,23 @@
+package Zad_4;
+//Stwórz klasę
+//        Storage , która będzie miała prywatne pole typu Map , publiczny konstruktor oraz metody:
+//        addToStorage(String key, String value)
+//        → dodawanie elementów do przechowywalni
+//        printValues(String key)
+//        → wyświetlanie wszystkich elementów pod danym kluczem
+//        findValues(String value)
+//        → wyświetlanie wszystkich kluczy, które mają podaną wartość
+//        Klasa
+//        Storage powinna pozwalać na przechowywanie wielu wartości pod jednym kluczem.
+
+import java.util.Map;
+
+public class Storage {
+
+    private static void Map{
+    }
+    public Storage(){
+
+    }
+
+}
