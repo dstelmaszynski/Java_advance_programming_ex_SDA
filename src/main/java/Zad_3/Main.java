a5b201da13a37bc7dbf32e83a3154986d03ecbf8
Dominik <stelmaszynski.dominik@gmail.com>
2020-07-08 19:34:56 +0200

    Ex_3 Map done

--- src/main/java/Zad_3/Main.java
+++ src/main/java/Zad_3/Main.java
@@ -1,7 +1,35 @@
 package Zad_3;
+//Stwórz metodę, która jako parametr przyjmuje mapę, 
+// gdzie kluczem jest string, a wartością liczba, 
+// a następnie wypisuje każdy element mapy do konsoli w formacie: Klucz: <k>, Wartość: <v> v>. 
+// Na końcu każdego wiersza poza ostatnim, powinien być przecinek, a w ostatnim kropka.
+
+
+import java.security.Key;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
+        //obiekt mapy
+        Map<String, Number> firstMap = new HashMap<>();
 
+        //dodawanie do mapy
+        firstMap.put("Java", 18);
+        firstMap.put("Python", 1);
+        firstMap.put("PHP", 0);
+
+        //wywolanie metody
+        printMap(firstMap);
+
+    }
+
+    //metoda - zawsze nazywana czasownikami
+    public static void printMap(Map<String, Number> map) {
+
+        //iterowanie po mapie
+        for (Map.Entry entry : map.entrySet()) {
+            System.out.println("Klucz: " + entry.getKey() + " ||" + " Wartosc: " + entry.getValue());
+        }
     }
 }
