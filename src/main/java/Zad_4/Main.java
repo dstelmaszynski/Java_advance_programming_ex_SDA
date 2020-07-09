50887fbbcd8a3951ed8b6bd11658641ef6451edd
Dominik <stelmaszynski.dominik@gmail.com>
2020-07-09 18:35:54 +0200

    addToStorage and printValues

--- src/main/java/Zad_4/Main.java
+++ src/main/java/Zad_4/Main.java
@@ -1,7 +1,21 @@
 package Zad_4;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
+
+        Map<String, String> someMap = new HashMap<>();
+        someMap.put("Java", "one");
+        someMap.put("C++", "two");
+        someMap.put("Python", "three");
+
+        Storage storage = new Storage(someMap);
+        storage.printValues("Javak");
+
+        storage.addToStorage("HTML", "four");
+        storage.printValues("HTML");
 
     }
 }
--- src/main/java/Zad_4/Storage.java
+++ src/main/java/Zad_4/Storage.java
@@ -1,23 +1,40 @@
 package Zad_4;
-//Stwórz klasę
-//        Storage , która będzie miała prywatne pole typu Map , publiczny konstruktor oraz metody:
+//Stwórz klasę Storage , która będzie miała prywatne pole typu Map ,
+// publiczny konstruktor oraz metody:
 //        addToStorage(String key, String value)
 //        → dodawanie elementów do przechowywalni
 //        printValues(String key)
 //        → wyświetlanie wszystkich elementów pod danym kluczem
 //        findValues(String value)
 //        → wyświetlanie wszystkich kluczy, które mają podaną wartość
-//        Klasa
-//        Storage powinna pozwalać na przechowywanie wielu wartości pod jednym kluczem.
+//        Klasa Storage powinna pozwalać na przechowywanie wielu wartości pod jednym kluczem.
 
 import java.util.Map;
 
 public class Storage {
 
-    private static void Map{
-    }
-    public Storage(){
+    private Map<String, String> map;
 
+    //konstruktor
+    public Storage(Map<String, String> map) {
+
+        this.map = map;
     }
 
+    //metody
+    public void addToStorage(String key, String value) {
+        map.put(key, value);
+    }
+
+    public void printValues(String key) {
+        if (!map.containsKey(key)) { //negacja dla kluczy, ktorych nie ma w mapie
+            System.out.println("This key doesn't exist: " + key);
+        } else { //zeby nie wyswielac nulla
+            String value = map.get(key);
+            System.out.println(value);
+        }
+
+//        public void findValues (String value){
+//        }
+    }
 }
