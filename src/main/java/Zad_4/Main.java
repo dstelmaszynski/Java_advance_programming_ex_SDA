e91e17bdb93c380fd1f9fc4f5e1606ee1b5c8a11
Dominik <stelmaszynski.dominik@gmail.com>
2020-07-10 19:19:26 +0200

    Ex 4. last task

--- src/main/java/Zad_4/Main.java
+++ src/main/java/Zad_4/Main.java
@@ -6,19 +6,32 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
 
-        Map<String, String> someMap = new HashMap<>();
-        someMap.put("Java", "one");
-        someMap.put("C++", "two");
-        someMap.put("Python", "three");
 
-        Storage storage = new Storage(someMap);
-        storage.printValues("Javak");
+        Storage storage = new Storage();
+        storage.addToStorage("Java", "one");
+        storage.addToStorage("Java", "two");
+        storage.addToStorage("Python", "two");
 
-        storage.addToStorage("HTML", "four");
-        storage.printValues("HTML");
-        storage.addToStorage("Java", "three");
+        storage.printValues("two");
+        System.out.println("_______________________");
+        storage.findValues("Java");
 
-        storage.findValues("three");
+        //pierwsza implementacja poniżej. wyzej zmiana dot.
+        // przechowywania wielu wartosci pod jednym kluczem
+
+//        Map<String, String> someMap = new HashMap<>();
+//        someMap.put("Java", "one");
+//        someMap.put("C++", "two");
+//        someMap.put("Python", "three");
+//
+//        Storage storage = new Storage(someMap);
+//        storage.printValues("Javak");
+//
+//        storage.addToStorage("HTML", "four");
+//        storage.printValues("HTML");
+//        storage.addToStorage("Java", "three");
+//
+//        storage.findValues("three");
 
     }
 }
--- src/main/java/Zad_4/Storage.java
+++ src/main/java/Zad_4/Storage.java
@@ -9,42 +9,69 @@ package Zad_4;
 //        → wyświetlanie wszystkich kluczy, które mają podaną wartość
 //        Klasa Storage powinna pozwalać na przechowywanie wielu wartości pod jednym kluczem.
 
-import org.w3c.dom.ls.LSOutput;
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class Storage {
 
-    private Map<String, String> map;
+    private Map<String, List<String>> map = new HashMap<>();
+
+    public Storage(){
+
+    }
 
     //konstruktor
-    public Storage(Map<String, String> map) {
-
+    public Storage(Map<String, List<String>> map) { //lista jako parametr, zeby klasa Storage mogla
+                                                    //przechowywac wiele wartosci pod jednym kluczem
         this.map = map;
     }
 
     //metody
     public void addToStorage(String key, String value) {
-        map.put(key, value);
-    }
-
-    public void printValues(String key) {
-        if (!map.containsKey(key)) { //negacja dla kluczy, ktorych nie ma w mapie
-            System.out.println("This key doesn't exist: " + key);
-        } else { //zeby nie wyswielac nulla
-            String value = map.get(key);
-            System.out.println(value);
+        if (!map.containsKey(key)){
+            List<String> values = new ArrayList<>();
+            values.add(value);
+            map.put(key, values);
+        } else {
+            map.get(key).add(value);
         }
     }
+
+    //ponizej pierwsza implementacja
+
+//    public void printValues(String key) {
+//        if (!map.containsKey(key)) { //negacja dla kluczy, ktorych nie ma w mapie
+//            System.out.println("This key doesn't exist: ");
+//        } else { //zeby nie wyswielac nulla
+//            List<String> value = map.get(key);
+//            System.out.println(value);
+//        }
+//    }
+
+        public void printValues(String key) {
+        if (!map.containsKey(key)) { //negacja dla kluczy, ktorych nie ma w mapie
+            System.out.println("This key doesn't exist: " + key);
+            return;
+        } List <String> value = map.get(key);
+            System.out.println(value);
+    }
     public void findValues(String value) {
-        if (!map.containsValue(value)) { //negacja dla kluczy, ktorych nie ma w mapie
+        if (!isValuePresentInMap(value)) { //negacja dla kluczy, ktorych nie ma w mapie
             System.out.println("This value doesn't exist: " + value);
             return;  //zeby nie wyswielac nulla (2 opcja - bez else)
         }
         map.entrySet()
                 .stream()
-                .filter(entry -> value.equals(entry.getValue()))
+//                .filter(entry -> entry.getValue().contains(value))
                 .forEach(entry -> System.out.println(entry));
+    }
+
+    private boolean isValuePresentInMap (String value){
+        return map.entrySet()
+                .stream()
+                .anyMatch(entry -> entry.getValue().contains(value));
+
     }
 }
